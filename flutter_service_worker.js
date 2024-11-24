1180f1533f52dbb3298ebfb47ff4506baee5a53b
Phahmed89 <ph.ahmedj@gmail.com>
2024-11-24 22:48:31 +0300

    Add files via upload

---
+++ flutter_service_worker.js
@@ -0,0 +1,251 @@
+'use strict';
+const MANIFEST = 'flutter-app-manifest';
+const TEMP = 'flutter-temp-cache';
+const CACHE_NAME = 'flutter-app-cache';
+
+const RESOURCES = {"assets/AssetManifest.bin": "2aa9f813bb7a7f49b1739997aba4ad9d",
+"assets/AssetManifest.bin.json": "3879b59f315cfd78619818e6ee82c79f",
+"assets/AssetManifest.json": "f4ad42f2da63eb25e4b30a2ec42e1338",
+"assets/assets/audios/favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"assets/assets/fonts/favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"assets/assets/fonts/Linke.ttf": "aa3832163b621ddeafde20c9b4865d11",
+"assets/assets/fonts/MyFlutterApp.ttf": "a87cc2493d7ec73839393347f2c612dd",
+"assets/assets/images/1.png": "239e533d3ecdb6ac9db53d60147d9aca",
+"assets/assets/images/2.png": "88f9c59bf60accdbe679ce6ee742ceab",
+"assets/assets/images/20210628130300313_ggxd1ldh_0ne.png": "6f6f893a0376d6a57a99f9622546f77a",
+"assets/assets/images/20211007070545693_qycb3bt4_nsc.png": "2d82242b714786ceb557adb4bc24b69c",
+"assets/assets/images/3.png": "cc42088eb0750bdfdd6bb578afe717ff",
+"assets/assets/images/4.png": "014b246c234651b6025206d126bbb0d4",
+"assets/assets/images/5.png": "6a0aca908a2f54cfcf9e46a599f09862",
+"assets/assets/images/AMERICAN-HOSPITAL_0.png": "aeefbac2b778041730b4b1380a3f8a55",
+"assets/assets/images/day2daynutrional_supplement-logo-png.png": "2fdd83026879ceaf606f0248692a6de9",
+"assets/assets/images/digital-7641873.jpg": "a50b817148d5a03492422d5098459fd5",
+"assets/assets/images/favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"assets/assets/images/financial.jpg": "1572851fcd0dfbfc14f449d7cf500d2c",
+"assets/assets/images/f_and_b.jpg": "3227f6784df92c0554ea5d19a4703e5c",
+"assets/assets/images/healthcare.jpg": "3842e1b1b086430895ce24dad62ab511",
+"assets/assets/images/logo_blackpng.png": "041959774a257cb70b913f2b00cb9f01",
+"assets/assets/images/marketing.jpg": "a6436374e6189dda2961445c57da3cfc",
+"assets/assets/images/retail.jpg": "1fd0d79c28dece72f7771bb567c83c93",
+"assets/assets/images/salmaniya_gate.png": "b0c7e8af0b4c8b12bd1446c67544b4d9",
+"assets/assets/images/socialpng.png": "5508656ceb393b58776c6c3af7fb17b5",
+"assets/assets/images/technology.jpg": "9f46af46975c2181dda518afc4021abc",
+"assets/assets/jsons/favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"assets/assets/pdfs/favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"assets/assets/rive_animations/favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"assets/assets/videos/favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"assets/FontManifest.json": "464f244d01edf0a7336c4d2d67b78aca",
+"assets/fonts/MaterialIcons-Regular.otf": "3442a9741f574a2b2efd0e3f64591300",
+"assets/NOTICES": "d5463d62b3474e104ebcf7ff40dee009",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf": "17ee8e30dde24e349e70ffcdc0073fb0",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf": "f3307f62ddff94d2cd8b103daf8d1b0f",
+"assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf": "04f83c01dded195a11d21c2edf643455",
+"assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
+"canvaskit/canvaskit.js": "66177750aff65a66cb07bb44b8c6422b",
+"canvaskit/canvaskit.js.symbols": "48c83a2ce573d9692e8d970e288d75f7",
+"canvaskit/canvaskit.wasm": "1f237a213d7370cf95f443d896176460",
+"canvaskit/chromium/canvaskit.js": "671c6b4f8fcc199dcc551c7bb125f239",
+"canvaskit/chromium/canvaskit.js.symbols": "a012ed99ccba193cf96bb2643003f6fc",
+"canvaskit/chromium/canvaskit.wasm": "b1ac05b29c127d86df4bcfbf50dd902a",
+"canvaskit/skwasm.js": "694fda5704053957c2594de355805228",
+"canvaskit/skwasm.js.symbols": "262f4827a1317abb59d71d6c587a93e2",
+"canvaskit/skwasm.wasm": "9f0c0c02b82a910d12ce0543ec130e60",
+"canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
+"favicon.png": "2704101cb06ce66e2000356a312be25c",
+"flutter.js": "f393d3c16b631f36852323de8e583132",
+"flutter_bootstrap.js": "39fb45d757cf7864bed668ac788d0b1e",
+"icons/android-chrome-192x192.png": "5b885e315695a79e546561f0996c2821",
+"icons/android-chrome-512x512.png": "ae8098913990e9ba8523aec6fd6f846f",
+"icons/apple-touch-icon.png": "a703fc0334153f464cc8ce877d0e51a6",
+"icons/RX%20website/earth2.png": "3e51ccddc898bc90db219fccf98e335d",
+"icons/RX%20website/logo%20blackpng.png": "041959774a257cb70b913f2b00cb9f01",
+"icons/RX%20website/logo%20whitepng.png": "ca936a0b9c4d953d38f4d049ce0470e7",
+"icons/RX%20website/RX%20Consultancy%20Web.png": "85d0d98b56f70f0e50e3777c72543235",
+"icons/RX%20website/Screenshot%202024-11-06%20125805.png": "378a260e36d2e8b23a85c3df2338d4e0",
+"icons/RX%20website/Screenshot%202024-11-07%20144724.png": "4ae704b9af0cd58f39e2423f24734646",
+"icons/RX%20website/website%20assets/20210628130300313_ggxd1ldh_0ne.png": "6f6f893a0376d6a57a99f9622546f77a",
+"icons/RX%20website/website%20assets/20211007070545693_qycb3bt4_nsc.png": "2d82242b714786ceb557adb4bc24b69c",
+"icons/RX%20website/website%20assets/AMERICAN-HOSPITAL_0.png": "aeefbac2b778041730b4b1380a3f8a55",
+"icons/RX%20website/website%20assets/day2daynutrional%20supplement-logo-png.png": "2fdd83026879ceaf606f0248692a6de9",
+"icons/RX%20website/website%20assets/earth.jpg": "3dd85690c85e57f3eac13dc8ca739ae1",
+"icons/RX%20website/website%20assets/image4.png": "2fdd83026879ceaf606f0248692a6de9",
+"icons/RX%20website/website%20assets/logoBlack.png": "5877afa0c821bf8334abaf66704d5a44",
+"icons/RX%20website/website%20assets/salmaniya%20gate.png": "b0c7e8af0b4c8b12bd1446c67544b4d9",
+"icons/RX%20website/website%20assets/socialpng.png": "5508656ceb393b58776c6c3af7fb17b5",
+"index.html": "f5d71488aefbfb27bccba19071f172ff",
+"/": "f5d71488aefbfb27bccba19071f172ff",
+"logo.png": "5b885e315695a79e546561f0996c2821",
+"main.dart.js": "86ee45bbb564364ea4d7f2128a5a3454",
+"socialpng.png": "5508656ceb393b58776c6c3af7fb17b5",
+"version.json": "980547175e325fe622a3362b84d55b6a"};
+// The application shell files that are downloaded before a service worker can
+// start.
+const CORE = ["main.dart.js",
+"index.html",
+"flutter_bootstrap.js",
+"assets/AssetManifest.bin.json",
+"assets/FontManifest.json"];
+
+// During install, the TEMP cache is populated with the application shell files.
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+  return event.waitUntil(
+    caches.open(TEMP).then((cache) => {
+      return cache.addAll(
+        CORE.map((value) => new Request(value, {'cache': 'reload'})));
+    })
+  );
+});
+// During activate, the cache is populated with the temp files downloaded in
+// install. If this service worker is upgrading from one with a saved
+// MANIFEST, then use this to retain unchanged resource files.
+self.addEventListener("activate", function(event) {
+  return event.waitUntil(async function() {
+    try {
+      var contentCache = await caches.open(CACHE_NAME);
+      var tempCache = await caches.open(TEMP);
+      var manifestCache = await caches.open(MANIFEST);
+      var manifest = await manifestCache.match('manifest');
+      // When there is no prior manifest, clear the entire cache.
+      if (!manifest) {
+        await caches.delete(CACHE_NAME);
+        contentCache = await caches.open(CACHE_NAME);
+        for (var request of await tempCache.keys()) {
+          var response = await tempCache.match(request);
+          await contentCache.put(request, response);
+        }
+        await caches.delete(TEMP);
+        // Save the manifest to make future upgrades efficient.
+        await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+        // Claim client to enable caching on first launch
+        self.clients.claim();
+        return;
+      }
+      var oldManifest = await manifest.json();
+      var origin = self.location.origin;
+      for (var request of await contentCache.keys()) {
+        var key = request.url.substring(origin.length + 1);
+        if (key == "") {
+          key = "/";
+        }
+        // If a resource from the old manifest is not in the new cache, or if
+        // the MD5 sum has changed, delete it. Otherwise the resource is left
+        // in the cache and can be reused by the new service worker.
+        if (!RESOURCES[key] || RESOURCES[key] != oldManifest[key]) {
+          await contentCache.delete(request);
+        }
+      }
+      // Populate the cache with the app shell TEMP files, potentially overwriting
+      // cache files preserved above.
+      for (var request of await tempCache.keys()) {
+        var response = await tempCache.match(request);
+        await contentCache.put(request, response);
+      }
+      await caches.delete(TEMP);
+      // Save the manifest to make future upgrades efficient.
+      await manifestCache.put('manifest', new Response(JSON.stringify(RESOURCES)));
+      // Claim client to enable caching on first launch
+      self.clients.claim();
+      return;
+    } catch (err) {
+      // On an unhandled exception the state of the cache cannot be guaranteed.
+      console.error('Failed to upgrade service worker: ' + err);
+      await caches.delete(CACHE_NAME);
+      await caches.delete(TEMP);
+      await caches.delete(MANIFEST);
+    }
+  }());
+});
+// The fetch handler redirects requests for RESOURCE files to the service
+// worker cache.
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+  var origin = self.location.origin;
+  var key = event.request.url.substring(origin.length + 1);
+  // Redirect URLs to the index.html
+  if (key.indexOf('?v=') != -1) {
+    key = key.split('?v=')[0];
+  }
+  if (event.request.url == origin || event.request.url.startsWith(origin + '/#') || key == '') {
+    key = '/';
+  }
+  // If the URL is not the RESOURCE list then return to signal that the
+  // browser should take over.
+  if (!RESOURCES[key]) {
+    return;
+  }
+  // If the URL is the index.html, perform an online-first request.
+  if (key == '/') {
+    return onlineFirst(event);
+  }
+  event.respondWith(caches.open(CACHE_NAME)
+    .then((cache) =>  {
+      return cache.match(event.request).then((response) => {
+        // Either respond with the cached resource, or perform a fetch and
+        // lazily populate the cache only if the resource was successfully fetched.
+        return response || fetch(event.request).then((response) => {
+          if (response && Boolean(response.ok)) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      })
+    })
+  );
+});
+self.addEventListener('message', (event) => {
+  // SkipWaiting can be used to immediately activate a waiting service worker.
+  // This will also require a page refresh triggered by the main worker.
+  if (event.data === 'skipWaiting') {
+    self.skipWaiting();
+    return;
+  }
+  if (event.data === 'downloadOffline') {
+    downloadOffline();
+    return;
+  }
+});
+// Download offline will check the RESOURCES for all files not in the cache
+// and populate them.
+async function downloadOffline() {
+  var resources = [];
+  var contentCache = await caches.open(CACHE_NAME);
+  var currentContent = {};
+  for (var request of await contentCache.keys()) {
+    var key = request.url.substring(origin.length + 1);
+    if (key == "") {
+      key = "/";
+    }
+    currentContent[key] = true;
+  }
+  for (var resourceKey of Object.keys(RESOURCES)) {
+    if (!currentContent[resourceKey]) {
+      resources.push(resourceKey);
+    }
+  }
+  return contentCache.addAll(resources);
+}
+// Attempt to download the resource online before falling back to
+// the offline cache.
+function onlineFirst(event) {
+  return event.respondWith(
+    fetch(event.request).then((response) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, response.clone());
+        return response;
+      });
+    }).catch((error) => {
+      return caches.open(CACHE_NAME).then((cache) => {
+        return cache.match(event.request).then((response) => {
+          if (response != null) {
+            return response;
+          }
+          throw error;
+        });
+      });
+    })
+  );
+}
